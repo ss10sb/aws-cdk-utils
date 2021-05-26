e1ab68e11deb4579932145a120fce77af7403245
Scott Morken <scott.morken@phoenixcollege.edu>
2021-05-26 13:57:27 -0700

    more vpc lookup options

--- src/vpc-utils.ts
+++ src/vpc-utils.ts
@@ -1,4 +1,4 @@
-import {IVpc, Vpc} from "@aws-cdk/aws-ec2";
+import {IVpc, Vpc, VpcLookupOptions} from "@aws-cdk/aws-ec2";
 import {Stack} from "@aws-cdk/core";
 import {Config} from "./config";
 import {Utils} from "./utils";
@@ -6,10 +6,21 @@ import {Utils} from "./utils";
 export class VpcUtils {
 
     public static getVpcByName(stack: Stack, vpcName: string, prefix: string = 'stack'): IVpc {
-        return Vpc.fromLookup(stack, `${prefix}-vpc`, {
+        return this.getVpcByOptions(stack, {
             isDefault: false,
             vpcName: vpcName
-        });
+        }, prefix);
+    }
+
+    public static getVpcById(stack: Stack, vpcId: string, prefix: string = 'stack'): IVpc {
+        return this.getVpcByOptions(stack, {
+            isDefault: false,
+            vpcId: vpcId
+        }, prefix);
+    }
+
+    public static getVpcByOptions(stack: Stack, opts: VpcLookupOptions, prefix: string = 'stack'): IVpc {
+        return Vpc.fromLookup(stack, `${prefix}-vpc`, opts);
     }
 
     public static getDefaultVpcName(config: Config, name: string = 'vpc01'): string {
