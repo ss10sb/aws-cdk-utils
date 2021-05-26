63f04ad8765bee3118e50380c57406ed6d6d08a3
Scott Morken <scott.morken@phoenixcollege.edu>
2021-05-26 14:00:54 -0700

    more vpc lookup options

--- lib/types/vpc-utils.d.ts
+++ lib/types/vpc-utils.d.ts
@@ -1,7 +1,9 @@
-import { IVpc } from "@aws-cdk/aws-ec2";
+import { IVpc, VpcLookupOptions } from "@aws-cdk/aws-ec2";
 import { Stack } from "@aws-cdk/core";
 import { Config } from "./config";
 export declare class VpcUtils {
     static getVpcByName(stack: Stack, vpcName: string, prefix?: string): IVpc;
+    static getVpcById(stack: Stack, vpcId: string, prefix?: string): IVpc;
+    static getVpcByOptions(stack: Stack, opts: VpcLookupOptions, prefix?: string): IVpc;
     static getDefaultVpcName(config: Config, name?: string): string;
 }
